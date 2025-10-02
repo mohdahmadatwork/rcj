de62aeb221ede78a90aa9e83b59a0ebc46297890
mohdahmad9652 <128692033+mohdahmad9652@users.noreply.github.com>
2025-10-02 08:06:09 +0530

    added default value

--- jewelry_orders/settings.py
+++ jewelry_orders/settings.py
@@ -272,9 +272,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER',default='royalcraftjewelers@gmail.com')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD',default='ABC')
+DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER',default='royalcraftjewelers')
 
 # Celery Configuration
 CELERY_BROKER_URL = config('REDIS_URL', default='redis://localhost:6379/0')
