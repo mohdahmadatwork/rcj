5fa5fd61dc2c2c60aba981250a481ce954385f3a
mohdahmad9652 <128692033+mohdahmad9652@users.noreply.github.com>
2025-10-03 00:55:30 +0530

    railway deployment prep

--- jewelry_orders/settings.py
+++ jewelry_orders/settings.py
@@ -156,11 +156,11 @@ WSGI_APPLICATION = 'jewelry_orders.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'psql': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('PGDATABASE', default=''),
         'USER': config('PGUSER', default=''),
